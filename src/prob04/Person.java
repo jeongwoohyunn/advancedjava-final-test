76c89e1f3eee6eea77e3b6e5a77e7cd3132ceac1
bit <bit@DESKTOP-74J9BGV>
2019-01-04 19:03:12 +0900

    1345

--- src/prob04/Person.java
+++ src/prob04/Person.java
@@ -4,11 +4,19 @@ public class Person {
 	private static int numberOfPerson; // 전체 인구수
 	private int age;
 	private String name;
-	
+	static int count=0;
+
 	public Person( String name ){
+		age=12;
+		name = "";
+		
 	}
 	
 	public Person(int age, String name){
+		
+		numberOfPerson++;
+		this.age = age;
+		this.name = name;
 	}
 	
 	public int getAge() {
@@ -24,11 +32,12 @@ public class Person {
 		this.name = name;
 	}
 	public void selfIntroduce(){
-		
+		System.out.println("내 이름은 "+getName() +"이며, 나이는 "+getAge() +"살 입니다.");
 	}
 
 	static int getPopulation(){
 		// 전체 인구수를 return 함
+		return numberOfPerson+1;
 	}
 }
 
